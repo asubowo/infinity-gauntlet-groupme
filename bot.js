d37d314778fdba00796fc5945caac846adf81c0e
Andrew Subowo <andrew.subowo@gmail.com>
2019-03-19 21:54:34 -0400

    typo fix

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/snapsOutForHarambe$/;
   console.log(request);
-  if(request.text )//&& botRegex.test(request.text)) {
+  if (request.text ) {//&& botRegex.test(request.text)) {
     this.res.writeHead(200);
     balance();
     this.res.end();
