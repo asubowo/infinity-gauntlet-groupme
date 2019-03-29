b34fb9fe6c33a547bb1ffb1a6789928779e86cc1
Andrew Subowo <andrew@banasolutions.com>
2019-03-28 23:11:32 -0400

    stringify that shit

--- bot.js
+++ bot.js
@@ -83,7 +83,7 @@ function balance() {
     responseBuffer.on('end', function(chunk) {
       responseBuffer += chunk;
       //console.log(response);
-      var responseData = JSON.parse(responseBuffer);
+      var responseData = JSON.parse(JSON.stringify(responseBuffer));
       console.log(responseData);
       var user;
       var bannedIDs = [];
